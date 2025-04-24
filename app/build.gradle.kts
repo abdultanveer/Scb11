9ab29106c6af7f6eaa79dd625904c9c7095e139a
abdul <abdul.tanveer@gmail.com>
2025-04-24 12:47:56 +0530

    rooomdb -2 insertion bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,13 +37,14 @@ android {
 
 dependencies {
     //kapt ("androidx.room:room-compiler:2.6.0")
+    ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.room.common)
-    implementation(libs.androidx.room.compiler)
+   // implementation(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
